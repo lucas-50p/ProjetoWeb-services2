51e4606b416aaca581bae87672477fcd5f0209d6
lucas bueno <lucas-50p>
2020-06-15 15:33:46 -0300

    H2 database, test profile, JPA

--- src/main/java/com/lucas/springweb/entities/User.java
+++ src/main/java/com/lucas/springweb/entities/User.java
@@ -2,9 +2,17 @@ package com.lucas.springweb.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id//Chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
