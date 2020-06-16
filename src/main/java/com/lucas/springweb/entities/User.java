c9e73a18fd05fe8359010451cccf4c001f882192
lucas bueno <lucas-50p>
2020-06-16 12:36:03 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- src/main/java/com/lucas/springweb/entities/User.java
+++ src/main/java/com/lucas/springweb/entities/User.java
@@ -1,13 +1,18 @@
 package com.lucas.springweb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -19,8 +24,12 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy ="client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
+	
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -70,7 +79,13 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
+	/*Se for só uma collections é só get*/
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,7 +110,9 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+
+
 	
 	
 }
