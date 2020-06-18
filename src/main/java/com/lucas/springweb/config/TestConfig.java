d4d7f5411b48f7ef2a0fd5f9c3825ed66fd02083
lucas bueno <lucas-50p>
2020-06-18 14:24:00 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/lucas/springweb/config/TestConfig.java
+++ src/main/java/com/lucas/springweb/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucas.springweb.entities.Category;
 import com.lucas.springweb.entities.Order;
 import com.lucas.springweb.entities.OrderItem;
+import com.lucas.springweb.entities.Payment;
 import com.lucas.springweb.entities.Product;
 import com.lucas.springweb.entities.User;
 import com.lucas.springweb.entities.enums.OrderStatus;
@@ -89,6 +90,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 
